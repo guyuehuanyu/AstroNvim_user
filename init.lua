c6fa3f9786eb547b82641158bbde9497f7aba71c
ZhenyuHu <zhenyu.hu@qq.com>
2022-11-22 21:48:49 +0800

    去除自动格式化
    Signed-off-by: ZhenyuHu <zhenyu.hu@qq.com>

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 local config = {
+  lsp = {
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
+    },
+  },
 }
 return config
