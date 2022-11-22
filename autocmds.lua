35debeb7cbb34f89756a968c7da5022bcea58fe3
ZhenyuHu <zhenyu.hu@qq.com>
2022-11-22 22:42:23 +0800

    自动打开上次退出的位置
    Signed-off-by: ZhenyuHu <zhenyu.hu@qq.com>

--- autocmds.lua
+++ autocmds.lua
@@ -1,8 +1,16 @@
 
---[[
---TODO: copy init.lua this is unuseful
-vim.cmd [[
-autocmd BufRead * autocmd FileType <buffer> ++once
-  \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
-]]
---]]
+vim.api.nvim_create_autocmd("BufRead", {
+  group = vim.api.nvim_create_augroup("last_position", { clear = true }),
+  callback = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("lasosition", { clear = true }),
+      buffer = 0,
+      callback = function()
+        vim.cmd [[ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif ]]
+      end,
+      desc = "test",
+      once = true,
+    })
+  end,
+  desc = "test",
+})
