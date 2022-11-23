7ab06d406c9b39d9a4869bc8d1664454d5b741b7
ZhenyuHu <zhenyu.hu@qq.com>
2022-11-23 09:44:27 +0800

    添加GiSigns相关命令
    Signed-off-by: ZhenyuHu <zhenyu.hu@qq.com>

--- mappings.lua
+++ mappings.lua
@@ -27,9 +27,6 @@ local mappings = {
     },
     ["<leader>fo"] = { function() require("telescope.builtin").oldfiles() end, desc = "Search history" },
     ["<leader>fw"] = { function() require("telescope.builtin").grep_string() end, desc = "Search for word under cursor" },
-    -- NeoTree
-    ["<C-n>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
-    ["<C-p>"] = { function() require("aerial").toggle() end, desc = "Symbols outline" },
     ["<leader>lk"] = { function() require("telescope.builtin").keymaps() end, desc = "List keymaps" },
     ["<leader>ls"] = {
       function()
@@ -42,9 +39,39 @@ local mappings = {
       end,
       desc = "Search symbols",
     },
+    -- NeoTree
+    ["<C-n>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    ["<C-p>"] = { function() require("aerial").toggle() end, desc = "Symbols outline" },
+    -- Terminal
     ["<A-i>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
     ["<A-h>"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
     ["<A-v>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" },
+    -- GitSigns
+    ["<leader>gs"] = { function() require("gitsigns").stage_hunk() end, desc = "Stage git hunk" },
+    ["<leader>gu"] = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage git hunk" },
+
+    ["<leader>ga"] = { function() require("gitsigns").stage_buffer() end, desc = "Stage buffer git hunk" },
+    ["<leader>gU"] = { function() require("gitsigns").undo_stage_buffer() end, desc = "Stage buffer git hunk" },
+
+    ["<leader>gd"] = { function() require("gitsigns").diffthis() end, desc = "View git diff" },
+    ["[["] = { function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        require("gitsigns").prev_hunk()
+      end)
+      return "<Ignore>"
+    end, desc = "Previous git hunk" },
+    ["]]"] = { function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        require("gitsigns").next_hunk()
+      end)
+      return "<Ignore>"
+    end, desc = "Next git hunk" },
   },
   t = {
     ["<C-'>"] = false, -- maps.n["<F7>"]
