c7e1199099a49f00303e098f0c759dc7caf63956
ZhenyuHu <zhenyu.hu@qq.com>
2022-12-01 23:46:10 +0800

    解决tab名称不是顺序的问题
    Signed-off-by: ZhenyuHu <zhenyu.hu@qq.com>

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -12,14 +12,20 @@ local config = {
       if (len > 30) then
         path = "..." .. path:sub(len-30, -1)
       end
+      --TODO: buf中应该有一个tabpagenr的值
       if (buf.tabnr == vim.api.nvim_get_current_tabpage()) then
-        local name = tostring(buf.tabnr) .. ".~/" .. path .. "/"
+        local name = tostring(vim.fn.tabpagenr()) .. ".~/" .. path .. "/"
         userbufferline[buf.tabnr] = name
         return name
       else
         return userbufferline[buf.tabnr]
       end
 
+      -- name                | str        | the basename of the active file
+      -- path                | str        | the full path of the active file
+      -- bufnr (buffer only) | int        | the number of the active buffer
+      -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
+      -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
     end,
     max_name_length = 40,
   },
