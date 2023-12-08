7979ec9548ef16a099e7229a6188d8107078963c
ZhenyuHu <zhenyu.hu@qq.com>
2023-12-08 10:32:59 +0800

    lsp：去除保存自动格式化功能
    Signed-off-by: ZhenyuHu <zhenyu.hu@qq.com>

---
+++ init.lua
@@ -0,0 +1,19 @@
+local config = {
+  lsp = {
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
+    },
+  },
+
+  --cmp = {
+  --  tags = 1000,
+  --  nvim_lsp = 900,
+  --  luasnip = 750,
+  --  path = 500,
+  --  buffer = 250,
+  --}
+}
+return config
